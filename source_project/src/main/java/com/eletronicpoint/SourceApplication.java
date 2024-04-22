b583dd25c634cdcdf09e2cf63b093a3d19ae7229
Silvio Martins <silviosmsantos@gmail.com>
2024-04-22 14:50:49 -0300

    refinamento das funcionalidades

--- source_project/src/main/java/com/eletronicpoint/SourceApplication.java
+++ source_project/src/main/java/com/eletronicpoint/SourceApplication.java
@@ -30,16 +30,84 @@ public class SourceApplication {
 		//Employee employee1 = new Employee(1001L, "JOICY PEIXOTO DIAS", "abbccc", "gerente comercial");
 		//Employee employee2 = new Employee(1002L, "RODRIGO RODRIGUES DA SILVA", "xesqdl123", "aux. administrativo");
 
-		List<Register> registers = new ArrayList<>();
-		registers = pointRegisterDAO.findAll();
+		System.out.println("Bem vindo ao sistema de registro de horarios");
 
-		for(Register r : registers){
-			System.out.println(r);
+		while (true) {
+			System.out.println("Digite seu ID:");
+			Long id = sc.nextLong();
+
+			System.out.println("Digite sua senha:");
+			String password = sc.next();
+
+			if (employeeDAO.verfyCredentials(id, password) == true) {
+
+				Employee employee = employeeDAO.findById(id);
+
+				if (employee.getRole().equalsIgnoreCase("gerente comercial")) {
+					System.out.println("Você é um gerente. Deseja imprimir um relatório?");
+					System.out.println("Digite 1 para SIM ou 2 para NÃO");
+					int printReport = sc.nextInt();
+					if (printReport == 1) {
+						List<Register> registers = new ArrayList<>();
+						registers = pointRegisterDAO.findAll();
+						PDFgenerator pdFgenerator = new PDFgenerator();
+						pdFgenerator.generatePDF(registers, "C:\\Cli\\pdfRegister.pdf");
+						continue;
+					}
+				}
+
+				System.out.println("Digite 1 - ENTRADA do expediente");
+				System.out.println("Digite 2 - SAÍDA do expediente");
+				int option = sc.nextInt();
+
+				System.out.println("Deseja adicionar alguma justificativa?");
+				System.out.println("Digite 1 - se SIM");
+				System.out.println("Digite 2 - se NÃO");
+				int justfy = sc.nextInt();
+
+				if (justfy == 1) {
+					System.out.println("Digite sua justificativa: ");
+					String justification = sc.next();
+					if (option == 1) {
+						Register register = new Register(employee, LocalDateTime.now(), Type.valueOf("ENTRY"), justification);
+						pointRegisterDAO.save(register);
+						System.out.println("ponto registrado com sucesso!");
+
+					}
+					if (option == 2) {
+						Register register = new Register(employee, LocalDateTime.now(), Type.valueOf("EXIT"), justification);
+						pointRegisterDAO.save(register);
+						System.out.println("ponto registrado com sucesso!");
+
+					}
+
+				}
+				if (justfy == 2) {
+					if (option == 1) {
+						Register register = new Register(employee, LocalDateTime.now(), Type.valueOf("ENTRY"));
+						pointRegisterDAO.save(register);
+						System.out.println("ponto registrado com sucesso!");
+
+					}
+					if (option == 2) {
+						Register register = new Register(employee, LocalDateTime.now(), Type.valueOf("EXIT"));
+						pointRegisterDAO.save(register);
+						System.out.println("ponto registrado com sucesso!");
+
+					}
+				}
+
+			}
+
+			else {
+				System.out.println("Credenciais inválidas. Deseja sair? (Digite 's' para sair)");
+				String sair = sc.next();
+				if (sair.equalsIgnoreCase("s")) {
+					break; // Sai do loop
+				}
+			}
 		}
 
-		PDFgenerator pdFgenerator = new PDFgenerator();
-
-		pdFgenerator.generatePDF(registers, "C:\\Cli\\pdfRegister.pdf");
 	}
 
 }
--- source_project/src/main/java/com/eletronicpoint/services/PDFgenerator.java
+++ source_project/src/main/java/com/eletronicpoint/services/PDFgenerator.java
@@ -1,59 +1,77 @@
 package com.eletronicpoint.services;
 
-
 import com.eletronicpoint.entities.Register;
 import com.lowagie.text.*;
+import com.lowagie.text.Font;
 import com.lowagie.text.pdf.PdfPTable;
+import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfWriter;
 
-
+import java.awt.*;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class PDFgenerator {
 
+    private static Font smallFont = new Font(Font.TIMES_ROMAN, 10);
     public void generatePDF(List<Register> registers, String filePath) throws FileNotFoundException {
         Document document = new Document();
         PdfWriter.getInstance(document, new FileOutputStream(filePath));
 
         document.open();
 
-        // Add title and table headers
         document.add(new Paragraph("Register Report"));
-        document.add(new Paragraph("document generated in: " + formatLocalDateTime(LocalDateTime.now())));
+        document.add(new Paragraph("Document generated on: " + formatLocalDateTime(LocalDateTime.now()), smallFont));
         document.add(new Paragraph("\n"));
 
-        PdfPTable table = new PdfPTable(5); // Adjust column count as needed
+        PdfPTable table = new PdfPTable(6);
         table.setWidthPercentage(100);
 
-        table.addCell("ID");
-        table.addCell("Employee Name");
-        table.addCell("Date/Time");
-        table.addCell("Type");
-        table.addCell("Justification");
-
-        // Add data from registers list
-        for (Register register : registers) {
-            table.addCell(String.valueOf(register.getId()));
-            table.addCell(register.getEmployee().getName());
-            table.addCell(register.getDateEndTime().toString());
-            table.addCell(register.getType().toString());
-            table.addCell(register.getJustification());
+        float[] columnWidths = new float[]{0.6f, 1.8f, 1.2f, 1.2f, 0.8f, 1.4f};
+        try {
+            table.setWidths(columnWidths);
+        } catch (DocumentException e) {
+            e.printStackTrace();
         }
 
+
+        addTableHeader(table, "Register ID");
+        addTableHeader(table, "Employee");
+        addTableHeader(table, "Role");
+        addTableHeader(table, "Date/Time");
+        addTableHeader(table, "Type");
+        addTableHeader(table, "Justification");
+
+
+        for (Register register : registers) {
+            addTableCell(table, String.valueOf(register.getId()));
+            addTableCell(table, register.getEmployee().getName());
+            addTableCell(table, register.getEmployee().getRole());
+            addTableCell(table, formatLocalDateTime(register.getDateEndTime()));
+            addTableCell(table, register.getType().toString());
+            addTableCell(table, register.getJustification());
+        }
 
         document.add(table);
 
         document.close();
     }
 
+    private void addTableHeader(PdfPTable table, String header) {
+        PdfPCell cell = new PdfPCell(new Phrase(header, smallFont));
+        cell.setBackgroundColor(Color.LIGHT_GRAY);
+        table.addCell(cell);
+    }
+
+    private void addTableCell(PdfPTable table, String content) {
+        table.addCell(new Phrase(content, smallFont));
+    }
+
     private String formatLocalDateTime(LocalDateTime dateTime) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
         return dateTime.format(formatter);
     }
-
 }
